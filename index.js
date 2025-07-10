db311a2f7bc53cbbb79d78415fd6336ea20f705f
divyanshu40 <divyanshuprajapati40@gmail.com>
2025-07-10 21:31:09 +0530

    getAllProjects() function updated in index.js file

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function addProjects(data) {
 // function to get projects
 async function getAllProjects(userId) {
     let userDetails = await user.findById(userId);
-    let projects = await project.find({ _id: { $in: userDetails.projects.map(ele => ele._id)}});
+    let projects = await project.find({ _id: { $in: userDetails.projects.map(ele => ele._id)}}).populate("tasks");
     return projects;
 }
 
