4f03d273533767e3a959dae67869e08f44851b94
divyanshu40 <divyanshuprajapati40@gmail.com>
2025-07-15 12:33:20 +0530

    index.js file updated

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ async function addProjects(data) {
     }
 }
 
-// function to get projects
+// function to get projects by token details
 async function getAllProjects(userId) {
     let userDetails = await user.findById(userId).populate("projects").populate("tasks").populate("teams");
     let projects = await project.find({ _id: { $in: userDetails.projects.map(ele => ele._id)}}).populate("tasks");
     return projects;
 }
 
-// function to get user details
+// function to get user details by token details
 async function getUserDetails(userId) {
     let userData = await user.findById(userId).populate("projects").populate("tasks").populate("teams");
     if (! userData) {
@@ -47,7 +47,7 @@ async function getUserDetails(userId) {
     return userData ;
 }
 
-// function to fetch tasks
+// function to fetch tasks by token details
 async function getAllTasks(userId) {
     let tasks = await task.find({ owners: userId }).populate("project").populate("team").populate("owners");
     return tasks;
@@ -60,7 +60,7 @@ async function addNewTeam(teamData) {
     let addedTeam = await new team({...teamData, members: teamMemberIds }).save();
     return addedTeam;
 }
-// function to update user details
+// function to update user details by id
 async function updateUserDetails(userId, updatedData) {
     let updatedUserDetails = await user.findByIdAndUpdate(userId, updatedData, { new: true }).populate("tasks").populate("projects").populate("teams");
     if (! updatedUserDetails) {
@@ -102,7 +102,7 @@ async function getTaskDetailsById(taskId) {
     return taskDetails;
 }
 
-// function to update project details
+// function to update project details by id
 async function updateProjectDetails(projectId, updatedData) {
     let updatedProject = await project.findByIdAndUpdate(projectId, updatedData, { new: true }).populate("tasks");
     if (! updatedProject) {
@@ -111,7 +111,7 @@ async function updateProjectDetails(projectId, updatedData) {
     return updatedProject
 }
 
-// function to updated team details
+// function to updated team details by id
 async function updateTeamDetails(teamId, updatedData) {
     let updatedTeam = await team.findByIdAndUpdate(teamId, updatedData, { new: true }).populate("members");
     if ( ! updatedTeam) {
@@ -120,7 +120,7 @@ async function updateTeamDetails(teamId, updatedData) {
     return updatedTeam;
 }
 
-// function to update task details
+// function to update task details by id
 async function updateTaskDetails(taskId, updatedData) {
     let updatedTask = await task.findByIdAndUpdate(taskId, updatedData, { new: true }).populate("project").populate("team").populate("owners");
     if (! updatedTask) {
@@ -133,6 +133,33 @@ async function updateTaskDetails(taskId, updatedData) {
 async function addNewTask(taskData) {
     let addedTask = await new task(taskData).save();
     return addedTask;
+}
+
+// function to update user details by some user data
+async function updatedUserDetailsByData(userData, updatedData) {
+    let updatedUser = await user.findOneAndUpdate(userData, updatedData, { new: true }).populate("projects").populate("tasks").populate("teams");
+    if (! updatedUser) {
+        return null
+    }
+    return updatedUser;
+}
+
+// function to get user details by id
+async function getUserDetailsById(userId) {
+    let userDetails = await user.findById(userId);
+    if (! userDetails) {
+        return null;
+    }
+    return userDetails;
+}
+
+// function to update user details by id
+async function updateUserDetailsById(userId, userData) {
+    let updatedUser = await user.findByIdAndUpdate(userId, userData, { new: true }).populate("projects").populate("tasks").populate("teams");
+    if (! updatedUser) {
+        return null;
+    }
+    return updatedUser;
 }
 
 // Routes for authentication
@@ -161,8 +188,8 @@ app.post("/team/new", authenticateToken, async (req, res) => {
     }
 });
 
-// POST Route to update user details
-app.post("/user/details/update", authenticateToken, async (req, res) => {
+// POST Route to update user details (find user by token)
+app.post("/user/update", authenticateToken, async (req, res) => {
     let userId = req.user.id;
     let updatedData = req.body;
     try {
@@ -176,7 +203,7 @@ app.post("/user/details/update", authenticateToken, async (req, res) => {
     }
 });
 
-// POST route update project details
+// POST route update project details by id
 app.post("/project/update/:id", authenticateToken, async (req, res) => {
     let projectId = req.params.id;
     let updatedData = req.body;
@@ -191,7 +218,7 @@ app.post("/project/update/:id", authenticateToken, async (req, res) => {
     }
 });
 
-// POST route update team details
+// POST route update team details by id
 app.post("/team/update/:id", authenticateToken, async (req, res) => {
     let teamId = req.params.id;
     let updatedData = req.body;
@@ -206,7 +233,7 @@ app.post("/team/update/:id", authenticateToken, async (req, res) => {
     }
 });
 
-// POST route to update task details
+// POST route to update task details by id
 app.post("/task/update/:id", authenticateToken, async (req, res) => {
     let taskId = req.params.id;
     let updatedData = req.body;
@@ -232,7 +259,37 @@ app.post("/task/new", authenticateToken, async (req, res) => {
     }
 });
 
-// GET Route to fetch all projects
+// POST route to  update user details on the basis of some data
+app.post("/user/update/details",authenticateToken, async (req, res) => {
+    let userData = req.body.userData;
+    let updatedData = req.body.updatedData;
+    try {
+        let response = await updatedUserDetailsByData(userData, updatedData);
+        if (response === null) {
+            res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(response);
+    } catch(error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// POST route to update user details by user id
+app.post("/user/updated/:id", authenticateToken, async (req, res) => {
+    let userId = req.params.id;
+    let updatedData = req.body;
+    try {
+        let response = await getUserDetailsById(userId, updatedData);
+        if (response === null) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.status(200).json(response);
+    } catch(error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+// GET Route to fetch all projects of user by token details
 app.get("/projects", authenticateToken, async (req, res) => {
     let userId = req.user.id;
     try {
@@ -246,7 +303,7 @@ app.get("/projects", authenticateToken, async (req, res) => {
     }
 });
 
-// GET route to get user details
+// GET route to get user details by token details
 app.get("/user/details", authenticateToken, async (req, res) => {
     let userId = req.user.id;
     try {
@@ -260,7 +317,7 @@ app.get("/user/details", authenticateToken, async (req, res) => {
     }
 });
 
-// GET route to get all tasks of a user
+// GET route to get all tasks of a user by token details
 app.get("/tasks", authenticateToken, async (req, res) => {
     let userId = req.user.id;
     try {
@@ -323,6 +380,20 @@ app.get("/task/details/:id", authenticateToken, async (req, res) => {
         let response = await getTaskDetailsById(taskId);
         if (response === null) {
             return res.status(404).json({ message: "Task not found" });
+        }
+        return res.status(200).json(response);
+    } catch(error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// GET route to get user details by id
+app.get("/user/details/:id", authenticateToken, async (req, res) => {
+    let userId = req.params.id;
+    try {
+        let response = await getUserDetailsById(userId);
+        if (response === null) {
+            return res.status(404).json({ message: "User not found"});
         }
         return res.status(200).json(response);
     } catch(error) {
